0e90f2e82e45620338377ce375563f8c2db1177d
kyle_koh <ykoh728@gmail.com>
2018-10-30 22:03:54 +0900

    make AddBook component

--- booklist-backend/app.js
+++ booklist-backend/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 // connect to mlab database
 mongoose.connect('mongodb://kyle:test123@ds245523.mlab.com:45523/booklist_db');
