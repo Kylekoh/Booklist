c9e4766c4b17a364cd5116e8d7c8ef4ff233eb60
kyle_koh <ykoh728@gmail.com>
2018-10-30 16:05:06 +0900

    set initial setting for backend

---
+++ booklist-backend/schema/schema.js
@@ -0,0 +1,53 @@
+const graphql = require('graphql');
+const _= require('lodash');
+
+const { GraphQLObjectType,
+		GraphQLSchema,
+		GraphQLString,
+		GraphQLInt,
+		GraphQLID
+	} = graphql;
+
+
+// dummy data
+var books = [
+	{name:'Name of the Wind', genre:'Fantasy', id:'1'},
+	{name:'The Final Empire', genre:'Fantasy', id:'2'},
+	{name:'The Long Earth', genre:'Sci-Fi', id:'3'}
+]
+
+
+const BookType = new GraphQLObjectType({
+	name: 'Book',
+	fields: () => ({
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		genre: { type: GraphQLString }
+	})
+});
+
+const AuthorType = new GraphQLObjectType({
+	name: 'Author',
+	fields: () => ({
+		id: { type: GraphQLString },
+		name: { type: GraphQLString }
+	})
+})
+
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQueryType',
+	fields: {
+		book: {
+		  type: BookType,
+		  args: {id: { type: GraphQLString }},
+		  resolve(parent, args) {
+		  	// code to get date from DB / other source
+		  	return _.find(books, {id:args.id});
+		  }
+		} 
+	}
+})
+
+module.exports = new GraphQLSchema({
+	query: RootQuery
+})
